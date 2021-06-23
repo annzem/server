de2b9a602c6b8dd59f9fc21800172c75ab18b9bf
annzem <annzemlinsky@gmail.com>
2021-06-23 21:30:05 +0200

    add checkbox

--- src/main/java/com/company/MyServer.java
+++ src/main/java/com/company/MyServer.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,12 +12,25 @@ public class MyServer {
     private static ServerSocket serverSocket;
     private static InputStream input;
     private static OutputStream output;
+    static String xmlPath = "/home/anna/IdeaProjects/server_connection/src/main/resources/xmlRequest.html";
+    static String jQueryPath = "/home/anna/IdeaProjects/server_connection/src/main/resources/jQueryRequest.html";
 
     public static void main(String[] args) {
         connectToServer();
     }
 
+    public static String readFile(String path) {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(path);
+            return IOUtils.toString(fis, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
     public static void connectToServer() {
+
         try {
             serverSocket = new ServerSocket(8080);
             while (true) {
@@ -34,30 +48,32 @@ public class MyServer {
 
                 String path = stringBuffer.toString().split("\n")[0].split(" ")[1];
 
-//                FileInputStream fis = new FileInputStream("/home/anna/IdeaProjects/server_connection/src/main/resources/xmlRequest.html");
-                FileInputStream fis = new FileInputStream("/home/anna/IdeaProjects/server_connection/src/main/resources/jQueryRequest.html");
-                String html = IOUtils.toString(fis, "UTF-8");
-
-                String response = "HTTP/1.1 200 OK\n" +
-                        "Date: Mon, 23 May 2005 22:38:34 GMT\n" +
-                        "Content-Type: text/html; charset=UTF-8\n" +
-                        "Content-Length: " + html.length() + "\n" +
-                        "Accept-Ranges: bytes\n" +
-                        "Connection: close\n" +
-                        "\n" + html;
-
-                String response1 = "HTTP/1.1 200 OK\n" +
-                        "Content-Type: text/html; charset=UTF-8\n" +
-                        "Content-Length: " + lengthDate + "\n" +
-                        "Accept-Ranges: bytes\n" +
-                        "Connection: close\n" +
-                        "\n" +
-                        date;
-
                 if (path.equals("/")) {
+
+                    String html = readFile(xmlPath);
+
+                    String response = "HTTP/1.1 200 OK\n" +
+                            "Date: Mon, 23 May 2005 22:38:34 GMT\n" +
+                            "Content-Type: text/html; charset=UTF-8\n" +
+                            "Content-Length: " + html.length() + "\n" +
+                            "Accept-Ranges: bytes\n" +
+                            "Connection: close\n" +
+                            "\n" + html;
+
                     output.write(response.getBytes());
                 }
                 if (path.equals("/first")) {
+
+
+
+                    String response1 = "HTTP/1.1 200 OK\n" +
+                            "Content-Type: text/html; charset=UTF-8\n" +
+                            "Content-Length: " + lengthDate + "\n" +
+                            "Accept-Ranges: bytes\n" +
+                            "Connection: close\n" +
+                            "\n" +
+                            date;
+
                     output.write(response1.getBytes());
                 }
 
